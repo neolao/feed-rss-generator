20d1360e7dae75addb22c4610889ed8a97ed139d
neolao <contact@neolao.com>
2022-11-04 23:26:33 +0100

    up

--- server.js
+++ server.js
@@ -7,6 +7,32 @@ fastify.get(`/${config.basePath}`, async (request, reply) => {
   return {};
 });
 
+fastify.post(`/${config.basePath}/:name/items`, async (request, reply) => {
+  const { name } = request.params;
+  const { id, title, url, description } = request.body;
+
+  const directoryPath = `${__dirname}/feeds/`;
+  const filePath = `${directoryPath}/${name}.xml`;
+  await fs.promises.mkdir(directoryPath, { recursive: true });
+
+  const content = `<rss version="2.0">
+  <channel>
+      <title>${name}</title>
+      <item>
+        <title>${title}</title>
+        <link>${url}</link>
+        <description>${description}</description>
+      </item>
+  </channel>
+  </rss>`;
+  await fs.promises.writeFile(filePath, content);
+
+  reply
+    .code(201)
+    .header("Content-Type", "application/json; charset=utf-8")
+    .send(request.body);
+});
+
 fastify.listen({ port: 3000 }, function (err, address) {
   if (err) {
     fastify.log.error(err)
